e668e5b1a658ba3e333e031d8d9f413b98cf8213
XCODE89 <maph0989@gmail.com>
2023-07-12 01:51:51 -0400

    Contacte Me parte 2

--- portfolio/src/components/Contact/contact.jsx
+++ portfolio/src/components/Contact/contact.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import style from "./contact.module.css"
-import {BiPhone} from "react-icons/bi"
+import {BiPhone, BiEnvelope} from "react-icons/bi"
+import github from "../../images/logos/Github.png"
+import linked from "../../images/logos/LinkedIn.png"
+import foto from "../../images/desconocida.jpg"
 
 const Contact = () => {
     const [start, setStart] = useState(false)
@@ -12,7 +15,7 @@ const Contact = () => {
         <div className={style.container}>
             <div className={style.titleContainer}>
                 <h1 className={style.title}>
-                    CONTACTAME
+                    CONTÁCTAME
                 </h1>
             </div>
 
@@ -22,13 +25,26 @@ const Contact = () => {
                     <div className={style.secondBlock}>
                         <div className={start?style.linea:style.lineaOculta}></div>
                         <div className={style.infoContainer}>
-                            <div>foto</div>
-                            <div>{BiPhone}</div>
-                            <div>email</div>
+                            <div className={style.infoBox}>
+                                <img src={foto} className={style.photo}/>
+                            </div>
+                            <div className={style.infoBox}>
+                                <BiPhone size={40} color="#dae3e5"/>
+                                <p>989957042</p>
+                            </div>
+                            <div className={style.infoBox}>
+                                <BiEnvelope size={40} color="#dae3e5"/>
+                                <p>maph0989@gmail.com</p>
+
+                            </div>
                         </div>
-                        <div className={style.socialContiner}>
-                            <div>linkedin</div>
-                            <div>github</div>
+                        <div className={style.socialContainer}>
+                            <div className={style.imageBox}>
+                                <img src={linked} className={style.socialImg}/>
+                            </div>
+                            <div className={style.imageBox}>
+                                <img src={github} className={style.socialImg}/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,6 +53,7 @@ const Contact = () => {
             <div className={style.formMap}>
                 {/* <div>map</div> */}
                 <form className={style.formContainer}>
+                    <div className={style.formTitle}>Envíame un mensaje!</div>
                     <label htmlFor="name">Nombre:</label>
                     <input type="text" name="name" className={style.input}></input>
                     <label htmlFor="email">Correo:</label>
