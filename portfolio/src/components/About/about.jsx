6aa9fe8092ad48feec060bbffc915dfa513d9dc5
XCODE89 <maph0989@gmail.com>
2024-04-17 14:55:36 -0400

    skills

--- portfolio/src/components/About/about.jsx
+++ portfolio/src/components/About/about.jsx
@@ -8,6 +8,15 @@ import python from "../../images/logos/Python.png"
 import react from "../../images/logos/React.png"
 import django from "../../images/logos/Django.png"
 import Titulo from "../Titulo/titulo"
+import { IoMdCloudDownload } from "react-icons/io";
+import { FaReact, FaNodeJs, FaPython, FaCss3Alt, FaHtml5, FaGitAlt, FaBootstrap, FaDocker } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiMongodb, SiDjango, SiTailwindcss, SiExpress } from "react-icons/si";
+
+
+
+
 
 const About = () => {
     const [start, setStart] = useState(false)
@@ -17,46 +26,117 @@ const About = () => {
     },[])
     return (
         <div className={style.container}>
-            <div className={style.titleContainer}>
-                <Titulo titulo="SOBRE MI" inicial="S"/>
+            <div className={style.grid}>
+                <div className={style.titleContainer}>
+                    <Titulo titulo="SOBRE MI" inicial="S"/>
+                </div>
+                <div className={style.content}>
+                    <div>
+                        <p className={style.text}>
+                        ¡Hola! Soy Miguel, un desarrollador web full stack con un trasfondo diverso. Mi capacidad de adaptación se destaca en proyectos que reflejan mi rápido aprendizaje. Fusiono mi sólida formación con la tecnología, creando soluciones digitales impactantes. Además de programar, valoro habilidades blandas como comunicación efectiva y trabajo en equipo. Emocionado por aprender y contribuir a proyectos desafiantes que dejen huella en el mundo digital. 🚀✨
+                        </p>
+                    </div>
+                    <div className={style.buttons}>
+                        <button className={style.button}>
+                            <a href="./CV_DANTE_APONTE.pdf" download className={style.link}>
+                                <span className={style.buttonContent}>Descargar CV</span>
+                                <IoMdCloudDownload className={style.icon} size={35}/>
+
+                            </a>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className={style.content}>
-                <div>
-                    <p className={style.text}>
-                    ¡Hola! Soy Miguel, un desarrollador web full stack con un trasfondo diverso. Mi capacidad de adaptación se destaca en proyectos que reflejan mi rápido aprendizaje. Fusiono mi sólida formación con la tecnología, creando soluciones digitales impactantes. Además de programar, valoro habilidades blandas como comunicación efectiva y trabajo en equipo. Emocionado por aprender y contribuir a proyectos desafiantes que dejen huella en el mundo digital. 🚀✨
-                    </p>
+            <div className={style.techsContainer}>
+                <h2 className={style.title}>skills</h2>
+                <div className={style.skills}>
+                    <div className={style.skillBox}>
+                        <div className={style.tech}>
+                            <IoLogoJavascript className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Javascript</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <FaPython className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Python</h3>
+                            <h4 className={style.techLevel}>Basic</h4>
+                        </div>
+                    </div>
+
+                    <div className={style.skillBox}>
+                        <div className={style.tech}>
+                            <FaReact className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>React</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <FaCss3Alt className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>CSS</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <FaHtml5 className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>HTML</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <SiTailwindcss className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Tailwind</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <FaBootstrap className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Bootstrap</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                    </div>
+
+                    <div className={style.skillBox}>
+                        <div className={style.tech}>
+                            <FaNodeJs className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>NodeJS</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <SiExpress className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>ExpressJS</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <SiDjango className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Django</h3>
+                            <h4 className={style.techLevel}>Basic</h4>
+                        </div>
+                    </div>
+
+                    <div className={style.skillBox}>
+                        <div className={style.tech}>
+                            <BiLogoPostgresql className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>PostgreSQL</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <SiMongodb className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>MongoDB</h3>
+                            <h4 className={style.techLevel}>Basic</h4>
+                        </div>
+                    </div>
+
+                    <div className={style.skillBox}>
+                        <div className={style.tech}>
+                            <FaGitAlt className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Git</h3>
+                            <h4 className={style.techLevel}>Intermediate</h4>
+                        </div>
+                        <div className={style.tech}>
+                            <FaDocker className={style.image} color={"var(--title-color)"} size={40}/>
+                            <h3 className={style.techName}>Docker</h3>
+                            <h4 className={style.techLevel}>Basic</h4>
+                        </div>
+                    </div>
                 </div>
-                <div className={style.buttons}>
-                    <button className={style.button}>
-                        <a href="./CV_DANTE_APONTE.pdf" download className={style.link}>
-                            <span className={style.buttonContent}>Descargar CV</span>
-                        </a>
-                    </button>
-                </div>
-                <div className={style.techsContainer}>
-                    <div className={style.tech}>
-                        <img src={react} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={express} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={node} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={postgre} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={mongo} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={python} className={style.image}/>
-                    </div>
-                    <div className={style.tech}>
-                        <img src={django} className={style.image}/>
-                    </div>
-                    
-                </div>
+                
+
             </div>
         </div>
     )
